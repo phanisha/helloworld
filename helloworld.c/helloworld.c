b762905002c15ec72d00ca5964d778a5a31956d6
phanisha <phanicovenant89@outlook.com>
2017-07-10 14:40:28 -0700

    symbols are changed

--- helloworld.c/helloworld.c
+++ helloworld.c/helloworld.c
@@ -3,8 +3,9 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main()
+int main()&pa
 {
-prin("hello world")
+prin("hello world")**
+}
 
 
